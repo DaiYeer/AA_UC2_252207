b88793d62ce3703da90c193aa0ca65617bcfd6b1
castr <castr@10.232.16.137>
2025-03-19 11:29:56 -0700

    Se crea una nueva clase llamada Ordenamiento

--- AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/Ordenamiento.java
+++ AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/Ordenamiento.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package algoritmos.aa_uc2_castro;
 
 /**
  *
  * @author castr
  */
-public class AA_UC2_Castro {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Ordenamiento {
+    
 }
