8f584049d6fb88671e306471373208c75028a020
castr <castr@10.232.16.137>
2025-03-19 11:37:37 -0700

    Se agrega el metodo para implementar el ordenamiento por insercion.

--- AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/Ordenamiento.java
+++ AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/Ordenamiento.java
@@ -10,4 +10,22 @@ package algoritmos.aa_uc2_castro;
  */
 public class Ordenamiento {
     
+    /**
+     * Metodo para implementar el algoritmo de ordenamiento por insercion
+     */
+    
+    public static void insertionSort(int[]arr) {
+        int f = arr.length;
+        for(int i = 1; i < f; i++){
+            int key = arr[i];
+            int m = i - 1;
+            
+            //Se mueven los elementos del arreglo que son mayores a key una posicion adelante
+            while(m >= 0 && arr[m] > key){
+                arr[m + 1] = arr[m];
+                m = m - 1;
+            }
+            arr[m + 1] = key;
+        }
+    }
 }
