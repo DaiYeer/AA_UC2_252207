3b8dd33a0f8979410d220cc95c30a0cb4f0de415
castr <castr@10.232.16.137>
2025-03-19 11:27:25 -0700

    Se crea el proyecto

---
+++ AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/AA_UC2_Castro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package algoritmos.aa_uc2_castro;
+
+/**
+ *
+ * @author castr
+ */
+public class AA_UC2_Castro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
