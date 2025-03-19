fc48e4e5e029b43fdc145a20eaec32acc88a206d
castr <castr@10.232.16.137>
2025-03-19 11:49:55 -0700

    Se crea un array y tambien importamos Arrays.toString que nos mostrara los elementos originales y los elementos ya ordenados.

--- AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/Pruebas.java
+++ AA_UC2_Castro/src/main/java/algoritmos/aa_uc2_castro/Pruebas.java
@@ -4,10 +4,18 @@
  */
 package algoritmos.aa_uc2_castro;
 
+import java.util.Arrays;
+
 /**
  *
  * @author castr
  */
 public class Pruebas {
-    
+    public static void main(String[] args) {
+        int[] array = {12,11,97,2,16,42,9,4,77};
+        
+        System.out.println("Arreglo original: " + Arrays.toString(array));
+        Ordenamiento.insertionSort(array);
+        System.out.println("Arreglo ordenado: " + Arrays.toString(array));
+    }
 }
