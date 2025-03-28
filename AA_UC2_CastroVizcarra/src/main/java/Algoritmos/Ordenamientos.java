63c85f20856485ebc269fe7dfc4e3f77bdc59931
castr <castr@10.232.16.137>
2025-03-28 15:06:59 -0700

    Se implementa el selectionSort.

--- AA_UC2_CastroVizcarra/src/main/java/Algoritmos/Ordenamientos.java
+++ AA_UC2_CastroVizcarra/src/main/java/Algoritmos/Ordenamientos.java
@@ -10,4 +10,23 @@ package Algoritmos;
  */
 public class Ordenamientos {
     
+    public static void selectionSort(int[] array){
+        int n = array.length;
+        
+        for (int i = 0;i < n - 1; i++){
+            int minIdx = i;
+            
+            //se busca el elemento de menor valor
+            for (int j = i + 1; j < n; j++){
+                if (array[j] < array[minIdx]){
+                    
+                }
+            }
+            
+            //se intarcambia el elemento de menor valor con el elemento actual
+            int temp = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = temp;
+        }
+    }
 }
