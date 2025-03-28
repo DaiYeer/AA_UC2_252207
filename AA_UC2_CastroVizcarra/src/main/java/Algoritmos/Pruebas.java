61fbecd9c2dcd60dafa65bae121b309b088ef6b7
castr <castr@10.232.16.137>
2025-03-28 15:39:58 -0700

    Se crea un Array.

--- AA_UC2_CastroVizcarra/src/main/java/Algoritmos/Pruebas.java
+++ AA_UC2_CastroVizcarra/src/main/java/Algoritmos/Pruebas.java
@@ -4,10 +4,19 @@
  */
 package Algoritmos;
 
+import Algoritmos.Ordenamientos;
+import java.util.Arrays;
+
 /**
  *
  * @author castr
  */
 public class Pruebas {
-    
+    public static void main(String[] args) {
+        int[] array = {64,25,12,11,22};
+        
+        System.out.println("Arreglo original: " + Arrays.toString(array));
+        Ordenamientos.selectionSort(array);
+        System.out.println("Arreglo ordenado: " + Arrays.toString(array));
+    }
 }
